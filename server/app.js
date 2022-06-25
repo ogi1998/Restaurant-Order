6554eff9e5b91a2950181e31c426d90d76440bd0
ogi1998 <ogivuja@gmail.com>
2022-06-25 09:27:13 +0200

    created .env file and used it in server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 
 
@@ -9,8 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb://localhost:27017/restaurant';
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.DB;
+const PORT = process.env.PORT;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
