2ce1447f7b7885309cc492aade645a5e204d5c9f
ogi1998 <ogivuja@gmail.com>
2022-06-25 11:27:43 +0200

    created route, controller and Model for restaurant resource

--- server/app.js
+++ server/app.js
@@ -5,12 +5,17 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import restaurantRoutes from './routes/restaurants.js';
+
 const app = express();
+
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+
+app.use('/restaurants', restaurantRoutes);
 
 const CONNECTION_URL = process.env.DB;
 const PORT = process.env.PORT;
---
+++ server/models/Restaurant.js
@@ -0,0 +1,65 @@
+import mongoose, { Schema } from "mongoose";
+
+const restaurantSchema = mongoose.Schema({
+    name: {
+        type: String,
+        unique: [true, 'Name must be unique'],
+        required: [true, 'Name is required']
+    },
+    description: {
+        type: String,
+        unique: [true, 'Description must be unique'],
+        required: [true, 'Description is required'] 
+    },
+    deliveryTime: {
+        type: String, 
+        required: [true, 'Delivery time is required']
+    },
+    deliveryPrice: {
+        type: Number,
+        required: [true, 'Delivery price is required']
+    },
+    workStart: {
+        type: Number,
+        required: [true, 'Work start is required']
+    },
+    workEnd: {
+        type: Number,
+        required: [true, 'Work end is required']
+    },
+    active: {
+        type: Boolean,
+        required: [true, 'Active is required'],
+        default: true
+    },
+    meals: [
+        {
+            name: {
+                type: String,
+                required: [true, 'Meal name is required']
+            },
+            description: {
+                type: String,
+                required: [true, 'Meal description is required']
+            },
+            type: {
+                type: String,
+                required: [true, 'Meal type is required']
+            },
+            categories: [
+                {
+                    type: String,
+                    required: [true, 'Meal needs to have categories']
+                }
+            ],
+            price: {
+                type: Number,
+                required: [true, 'Meal needs to have price']
+            }
+        }
+    ]
+});
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+export default Restaurant;
