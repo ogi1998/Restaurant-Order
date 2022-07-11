598575f78ed35b40ef1b61703cacdd26a24722cb
ogi1998 <ogivuja@gmail.com>
2022-07-11 15:19:50 +0200

    changed path for restaurant images

--- client/src/components/functionality/RestaurantItem.js
+++ client/src/components/functionality/RestaurantItem.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const RestaurantItem = (props) => {
   return (
     <Link to={`/restaurants/${props.id}`} className={classes.restaurant__item}>
-      <img className={classes.restaurant__image} src={`http://localhost:5000${props.img}`} alt={props.name} />
+      <img className={classes.restaurant__image} src={`${props.img}`} alt={props.name} />
       <div className={classes.restaurant__content}>
       <h3 className={classes.restaurant__title}>{props.title}</h3>
       <p className={classes.restaurant__desc}>{props.desc}</p>
